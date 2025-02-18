34ea20d75f2b22775ed752113827f3d832551c45
heyrincesingh <princesingh3632@gmail.com>
2025-02-18 20:47:54 +0530

    Session 1 - Completed

---
+++ Python Mega Course/Exercise/Ex_10.py
@@ -0,0 +1,8 @@
+# Capitalize Text
+# The code you see in the code has an error in line 2. Fix it so the script successfully capitalizes the text of variable person and prints it out.
+
+# person = "sarah"
+# print(capitalize(person))
+
+person = "sarah"
+print(person.capitalize())
---
+++ Python Mega Course/Exercise/Ex_8.py
@@ -0,0 +1,9 @@
+# Infinite While Loops
+# Write a while loop which:
+
+# (1) has True as its condition, and
+
+# (2) prints out "Hello" repeatedly.
+
+while True:
+    print("Hello")
