44270711c2b97ac5cc05abce04854c70ce329cb9
heyrincesingh <princesingh3632@gmail.com>
2025-02-28 14:08:00 +0530

    [App 10]: Session 39 - Completed

---
+++ Python Mega Course/App-10/session_38/main.py
@@ -0,0 +1,64 @@
+import requests # type: ignore
+import selectorlib # type: ignore
+import smtplib, ssl
+import time, os
+
+from dotenv import load_dotenv                  # type: ignore
+
+load_dotenv() 
+
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = os.getenv("email_username")
+    password = os.getenv("email_password")
+    receiver = "princesingh3632@gmail.com"
+    context = ssl.create_default_context()
+    message = message
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print("Email was sent ")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + '\n')
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
